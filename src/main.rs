6c3728ffd36a6ed4de9f9ebae2c7a623b87a7076
Connor Onweller <onweller@udel.edu>
2020-04-29 16:22:29 -0400

    Return descriptions and their source

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ const NUM_BLOCKS: usize = 9;
 
 #[derive(Serialize, Deserialize)]
 struct MyResponse {
-    contents: Vec<String>
+    contents: Vec<(String, String)>
 }
 
 
@@ -24,18 +24,87 @@ fn assignment() -> Json<MyResponse> {
         
 }
 
-fn get_assignment() -> Vec<String> {
+enum Condition {
+    Ours,
+    Microsoft,
+    Control,
+}
+
+const DESCRIPTIONS: [(&str, &str, &str); NUM_BLOCKS] = [
+    (
+        "our description 1",
+        "microsoft's description 1",
+        "control description 1"
+    ), (
+        "our description 2",
+        "microsoft's description 2",
+        "control description 2"
+    ), (
+        "our description 3",
+        "microsoft's description 3",
+        "control description 3"
+    ), (
+        "our description 4",
+        "microsoft's description 4",
+        "control description 4"
+    ), (
+        "our description 5",
+        "microsoft's description 5",
+        "control description 5"
+    ), (
+        "our description 6",
+        "microsoft's description 6",
+        "control description 6"
+    ), (
+        "our description 7",
+        "microsoft's description 7",
+        "control description 7"
+    ), (
+        "our description 8",
+        "microsoft's description 8",
+        "control description 8"
+    ), (
+        "our description 9",
+        "microsoft's description 9",
+        "control description 9"
+    ),
+];
+
+fn get_assignment() -> Vec<(String, String)> {
+
     let mut rng = thread_rng();
-    let mut conditions: Vec<String> = ["us", "ms", "ctrl"]
+    let mut conditions: Vec<&Condition> = [
+        Condition::Ours,
+        Condition::Microsoft,
+        Condition::Control,
+    ]
         .iter()
         .cycle()
         .take(NUM_BLOCKS)
-        .map(|x| String::from(*x))
         .collect();
 
     conditions.shuffle(&mut rng);
 
     conditions
+        .iter()
+        .zip(DESCRIPTIONS.iter())
+        .map(|(cond, desc)| {
+            match cond {
+                Condition::Ours => return (
+                    String::from("Ours"),
+                    String::from(desc.0)
+                ),
+                Condition::Microsoft => return (
+                    String::from("Microsoft"),
+                    String::from(desc.1)
+                ),
+                Condition::Control => return (
+                    String::from("Control"),
+                    String::from(desc.2)
+                ),
+            }
+        })
+    .collect()
 }
 
 fn main() {
